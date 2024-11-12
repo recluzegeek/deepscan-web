e10a67c74da8d53acb1772b7019df370fb09708b
recluzegeek <recluzegeek@gmail.com>
2024-11-12 22:27:10 +0500

    Add CSRF middleware exception to inference routes to FastAPI

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
+        $middleware->validateCsrfTokens(except: [
+            'inference/*',
+        ]);
+
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
