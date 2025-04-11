9cfcb84cd684aaafcd4d724c163187688e629f7c
recluzegeek <recluzegeek@gmail.com>
2025-04-11 23:06:08 +0500

    feat: improve MinIO provisioning script with user management and bucket creation

--- vagrant/minio.sh
+++ vagrant/minio.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-ROOT_USER="rgkadmin"
-ROOT_PASSWORD="minio-secret-key-change-me"
+ROOT_USER="rgk_admin"
+ROOT_PASSWORD="minio_secret_key_change_me"
+MINIO_DEPLOYMENT_ALIAS="rgk_minio"
+NORMAL_USERNAME="laravel"
+NORMAL_USER_PASSWORD="%@typicalstrongpassword:)"
+BUCKET_NAME="deepscan"
 PORT=9001
 
 sudo dnf update -y
@@ -9,6 +13,10 @@ sudo dnf install wget -y
 # Install MinIO
 wget https://dl.min.io/server/minio/release/linux-amd64/archive/minio-20250408154124.0.0-1.x86_64.rpm -O minio.rpm
 sudo dnf install minio.rpm -y
+
+# Install MinIO cli
+wget https://dl.min.io/client/mc/release/linux-amd64/mcli-20250408153949.0.0-1.x86_64.rpm -O mcli.rpm
+sudo dnf install mcli.rpm -y
 
 # Create data directory and user
 sudo mkdir -p /mnt/data
@@ -31,6 +39,21 @@ EOT
 sudo systemctl daemon-reexec
 sudo systemctl daemon-reload
 sudo systemctl enable --now minio
+
+/usr/local/bin/mcli alias set $MINIO_DEPLOYMENT_ALIAS/ http://$(hostname -I | cut -d' ' -f2):9000 $ROOT_USER $ROOT_PASSWORD
+
+# add less privileged minio user
+## mc admin user add ALIAS ACCESSKEY SECRETKEY
+/usr/local/bin/mcli admin user add $MINIO_DEPLOYMENT_ALIAS $NORMAL_USERNAME $NORMAL_USER_PASSWORD
+
+# attach readwrite policy to the user
+## mc admin policy attach ALIAS readwrite --user=USERNAME
+# TODO: attach more strict policy, restricting access to specific bucket
+/usr/local/bin/mcli admin policy attach $MINIO_DEPLOYMENT_ALIAS readwrite --user=$NORMAL_USERNAME
+/usr/local/bin/mcli admin user list $MINIO_DEPLOYMENT_ALIAS
+
+# Create bucket
+/usr/local/bin/mcli mb --region=us-east-1 $MINIO_DEPLOYMENT_ALIAS/$BUCKET_NAME
 
 # Show status
 sudo systemctl status minio
